0ccc461a39364921a26199e98296d738fd4aeed1
Carla Godinez <carlareyesgo@gmail.com>
2023-02-14 12:14:27 -0700

    Estructura de directorios y ShopLayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,16 @@
 
+import Typography from '@mui/material/Typography';
 import { Inter } from '@next/font/google'
+import { ShopLayout } from '../components/layouts';
 
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
-      <h1>Teslo-Shop</h1>
-    </>
+   <ShopLayout title={'Teslo-Shop- Home'} pageDescription={'Encuentra los mejores productos de Teslo'}>
+    <Typography variant='h1' component='h1'>Tienda</Typography>
+    <Typography variant='h2' sx={{mb: 1}}>Todos los productos</Typography>
+   </ShopLayout>
   )
 }
