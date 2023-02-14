432a975a7b5be32cb36dd34aa908183f73b0c1c4
Carla Godinez <carlareyesgo@gmail.com>
2023-02-14 16:44:18 -0700

    Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
 
+import { Grid } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardActionArea from '@mui/material/CardActionArea';
+import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Inter } from '@next/font/google'
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -11,6 +16,25 @@ export default function Home() {
    <ShopLayout title={'Teslo-Shop- Home'} pageDescription={'Encuentra los mejores productos de Teslo'}>
     <Typography variant='h1' component='h1'>Tienda</Typography>
     <Typography variant='h2' sx={{mb: 1}}>Todos los productos</Typography>
+
+    <Grid container spacing={4}>
+      {
+        initialData.products.map(product => (
+          <Grid item xs={6} sm={4} key={product.slug}>
+            <Card>
+              <CardActionArea>
+                <CardMedia
+                component='img'
+                image={`products/${product.images[0]}`}
+                alt={product.title}
+                />
+              </CardActionArea>
+            </Card>
+          </Grid>
+        ))
+      }
+    </Grid>
+
    </ShopLayout>
   )
 }
