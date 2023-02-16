9c7a66bf36463bf711eaf2d4ecf9b3bf4ea81ed9
Carla Godinez <carlareyesgo@gmail.com>
2023-02-16 12:46:39 -0700

    Mostrar productos del carrito de compras

--- components/cart/CartList.tsx
+++ components/cart/CartList.tsx
@@ -1,20 +1,50 @@
 import { initialData } from '../../database/products';
-import { Typography } from '@mui/material';
+import { Box, Button, CardActionArea, CardMedia, Grid, Typography } from '@mui/material';
+import NextLink from 'next/link';
+import { borderRadius } from '@mui/system';
+import { ItemCounter } from '../ui';
 
-const productsInCart = 
-   [ initialData.products[0],
+const productsInCart =
+    [initialData.products[0],
     initialData.products[1],
     initialData.products[2],
-]
+    ]
 
 export const CartList = () => {
-  return (
-    <>
-    {
-        productsInCart.map(product => (
-            <Typography key={product.slug}>{product.title}</Typography>
-        ))
-    }
-    </>
-  )
+    return (
+        <>
+            {
+                productsInCart.map(product => (
+                    <Grid container spacing={2} sx={{ mb: 1 }} key={product.slug}>
+                        <Grid item xs={3}>
+                            <NextLink href="/product/slug" passHref>
+                                <CardActionArea>
+                                    <CardMedia image={`products/${product.images[0]}`}
+                                    component='img'
+                                    sx={{borderRadius:'5px'}}
+                                    />
+                                </CardActionArea>
+                            </NextLink>
+                        </Grid>
+                        <Grid item xs={7}>
+                            <Box display='flex' flexDirection='column'>
+                                <Typography variant='body1'>{product.title}</Typography>
+                                <Typography variant='body1'>Talla: <strong>M</strong></Typography>
+
+                                <ItemCounter></ItemCounter>
+                            </Box>
+                        </Grid>
+                        <Grid item xs={2} display='flex' alignItems='center' flexDirection='column'>
+                            <Typography variant='subtitle1'>
+                                {`$${product.price}`}
+                            </Typography>
+                            <Button variant='text' color='secondary'>
+                                Remover
+                            </Button>
+                        </Grid>
+                    </Grid>
+                ))
+            }
+        </>
+    )
 }
