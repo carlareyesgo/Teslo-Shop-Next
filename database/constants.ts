e6957864c5230bc17fb20bcae8b85832327b644c
Carla Godinez <carlareyesgo@gmail.com>
2023-02-17 10:37:30 -0700

    REST- Aplicar filtros a la consulta

---
+++ database/constants.ts
@@ -0,0 +1,5 @@
+import { WomanOutlined } from "@mui/icons-material";
+
+export const SHOP_CONSTANTS = {
+    validGenders: ['men','women','kid','unisex'],
+}
--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,4 +1,4 @@
-import { db } from '../../../database'
+import { db, SHOP_CONSTANTS } from '../../../database'
 import { Product } from '../../../models'
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { IProduct } from '../../../interfaces'
@@ -26,8 +26,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
 }
 
 const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const {gender = 'all'} = req.query
+    let condition = {}
+    if ( gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
+        condition = {gender}
+    }
     await db.connect()
-    const products = await Product.find().select('title images price inStock slug').lean()
+    const products = await Product.find(condition).select('title images price inStock slug').lean()
 
     await db.disconnect()
 
