cdc25679e4f22f8c71d6870323a8070b0df3e2ca
Carla Godinez <carlareyesgo@gmail.com>
2023-02-17 13:49:07 -0700

    <SWR - React Hooks for Dara Fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,22 +6,35 @@ import CardActionArea from '@mui/material/CardActionArea';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Inter } from '@next/font/google'
+import { NextPage } from 'next';
 import { ShopLayout } from '../components/layouts';
-import { initialData } from '../database/products';
 
+import useSWR from "swr"
+
+const fetcher = (...args: [key: string]) => fetch(...args).then(res => res.json())
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+const HomePage: NextPage = () => {
+
+  const { data, error, isLoading } = useSWR("/api/products", fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (isLoading) return <div>loading...</div>
+
+
+
   return (
    <ShopLayout title={'Teslo-Shop- Home'} pageDescription={'Encuentra los mejores productos de Teslo'}>
     <Typography variant='h1' component='h1'>Tienda</Typography>
     <Typography variant='h2' sx={{mb: 1}}>Todos los productos</Typography>
 
   <ProductList
-    products={initialData.products as any}
+    products={data}
   />
 
    </ShopLayout>
   )
 }
+
+export default HomePage
