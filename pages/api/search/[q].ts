b368cdf2d2701105c5c97c2617614483cf2b4f10
Carla Godinez <carlareyesgo@gmail.com>
2023-02-17 11:15:33 -0700

    REST- Busqueda de productos

---
+++ pages/api/search/[q].ts
@@ -0,0 +1,48 @@
+import { db } from '../../../database';
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { connect } from '../../../database/db';
+import { Product } from '@/models';
+import { IProduct } from '@/interfaces';
+
+type Data = 
+  |  {message: string}
+  |  IProduct[]
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+   
+    switch(req.method){
+        case 'GET':
+        return searchProducts(req,res)
+
+        default:
+            return res.status(400).json({
+                message:'Bad request'
+            })
+    }
+
+
+}
+
+const searchProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) =>{
+    let {q = ''} = req.query
+
+    if(q.length === 0){
+        return res.status(400).json({
+            message:'Debe de especificar el query de búsqueda'
+        })
+    }
+    q = q.toString().toLowerCase()
+
+    await db.connect()
+    const products = await Product.find({
+        $text: {$search: q}
+    }).lean()
+
+
+    await db.disconnect()
+
+    
+
+    return res.status(200).json(products)
+}
